520a86d749e806bdf15dd03cb5ed1334a14df5f0
Yosuke Horie <Yosuke.Horie@sony.com>
2025-01-11 16:22:46 +0900

    Remove outdated migration files and policies for database cleanup

--- supabase/migrations/20250105105935_lucky_ocean.sql
+++
@@ -1,7 +0,0 @@
--- Update valid_type constraint to include 'request' type
-ALTER TABLE contents 
-DROP CONSTRAINT IF EXISTS valid_type;
-
-ALTER TABLE contents
-ADD CONSTRAINT valid_type 
-CHECK (type IN ('issue', 'knowledge', 'request'));
